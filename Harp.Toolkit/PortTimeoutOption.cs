ddf4e4c68faac954dffef1f8c29849a2b2cfd335
glopesdev <g.lopes@neurogears.org>
2026-01-30 20:29:31 +0000

    Refactor toolkit into independent components
    The aim is to make it easier to add and maintain individual features.
    
    Common options have been refactored into separate classes for reuse.

---
+++ Harp.Toolkit/PortTimeoutOption.cs
@@ -0,0 +1,12 @@
+﻿using System.CommandLine;
+
+namespace Harp.Toolkit;
+
+public class PortTimeoutOption : Option<int?>
+{
+    public PortTimeoutOption()
+        : base("--timeout")
+    {
+        Description = "Specifies an optional timeout, in milliseconds, to receive a response from the device.";
+    }
+}
---
+++ Harp.Toolkit/UpdateFirmwareCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.CommandLine;
+using Bonsai.Harp;
+
+namespace Harp.Toolkit;
+
+public class UpdateFirmwareCommand : Command
+{
+    public UpdateFirmwareCommand()
+        : base("update", "Update the device firmware from a local HEX file.")
+    {
+        PortNameOption portNameOption = new();
+        Option<FileInfo> firmwarePathOption = new("--path")
+        {
+            Description = "Specifies the path of the firmware file to write to the device.",
+            Required = true
+        };
+
+        Option<bool> forceUpdateOption = new("--force")
+        {
+            Description = "Indicates whether to force a firmware update on the device regardless of compatibility."
+        };
+
+        Options.Add(portNameOption);
+        Options.Add(firmwarePathOption);
+        Options.Add(forceUpdateOption);
+        SetAction(async parseResult =>
+        {
+            var firmwarePath = parseResult.GetRequiredValue(firmwarePathOption);
+            var portName = parseResult.GetRequiredValue(portNameOption);
+            var forceUpdate = parseResult.GetValue(forceUpdateOption);
+
+            var firmware = DeviceFirmware.FromFile(firmwarePath.FullName);
+            Console.WriteLine($"{firmware.Metadata}");
+            ProgressBar.Write(0);
+            try
+            {
+                var progress = new Progress<int>(ProgressBar.Update);
+                await Bootloader.UpdateFirmwareAsync(portName, firmware, forceUpdate, progress);
+            }
+            finally { Console.WriteLine(); }
+        });
+    }
+}
