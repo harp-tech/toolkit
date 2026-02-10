12a453eaf7931a92cbc5b6bce9629f130b4c4f4b
glopesdev <g.lopes@neurogears.org>
2026-02-10 14:44:19 +0000

    Refactor toolkit into independent components
    The aim is to make it easier to add and maintain individual features.
    
    Common options have been refactored into separate classes for reuse.

---
+++ Harp.Toolkit/ListCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.CommandLine;
+using System.IO.Ports;
+
+namespace Harp.Toolkit;
+
+public class ListCommand : Command
+{
+    public ListCommand()
+        : base("list", "List all available system serial ports.")
+    {
+        SetAction(parseResult =>
+        {
+            var portNames = SerialPort.GetPortNames();
+            Console.WriteLine($"PortNames: [{string.Join(", ", portNames)}]");
+        });
+    }
+}
---
+++ Harp.Toolkit/PortNameOption.cs
@@ -0,0 +1,13 @@
+﻿using System.CommandLine;
+
+namespace Harp.Toolkit;
+
+public class PortNameOption : Option<string>
+{
+    public PortNameOption()
+        : base("--port")
+    {
+        Description = "Specifies the name of the serial port used to communicate with the device.";
+        Required = true;
+    }
+}
