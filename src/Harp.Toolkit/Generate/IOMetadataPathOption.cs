be6adb02f877526be25ce4cbf986c4c8355b6e76
glopesdev <g.lopes@neurogears.org>
2026-03-16 10:18:50 +0000

    Add interface and firmware generator commands
    The backend is implemented using the new Harp.Generators API so that
    generation can run directly from the toolkit without additional
    dependencies.

---
+++ src/Harp.Toolkit/Generate/IOMetadataPathOption.cs
@@ -0,0 +1,14 @@
+﻿using System.CommandLine;
+
+namespace Harp.Toolkit.Generate;
+
+public class IOMetadataPathOption : Option<FileInfo>
+{
+    public IOMetadataPathOption()
+        : base("--ios")
+    {
+        OptionValidation.AcceptExistingOnly(this);
+        Description = "The path to the file describing the device IO pins.";
+        DefaultValueFactory = _ => new FileInfo("ios.yml");
+    }
+}
