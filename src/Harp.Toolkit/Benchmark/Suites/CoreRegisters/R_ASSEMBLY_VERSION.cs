d0ae5806bb9e1a4c02449972010953039a69db6d
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-04-19 19:07:43 -0700

    Add R_UID tests

--- src/Harp.Toolkit/Benchmark/Suites/CoreRegisters/R_ASSEMBLY_VERSION.cs
+++ src/Harp.Toolkit/Benchmark/Suites/CoreRegisters/R_ASSEMBLY_VERSION.cs
@@ -7,7 +7,7 @@ internal class R_ASSEMBLY_VERSION : Suite
     public override string Description => "AssemblyVersion Register Tests";
 
     [HarpTest(Description = "Validates the deprecated register AssemblyVersion returns 0x00.")]
-    public async Task<IResult> IsReturnZero(string portName)
+    public async Task<IResult> AssertReturnsZero(string portName)
     {
         using (var device = new AsyncDevice(portName))
         {
@@ -15,7 +15,7 @@ internal class R_ASSEMBLY_VERSION : Suite
             bool isZero = value == 0x00;
             return new AssertionResult(
                 isZero,
-                isZero => isZero ?
+                x => x ?
                     $"AssemblyVersion register correctly returned 0x00." :
                     $"AssemblyVersion register returned a non-zero value (0x{value:X2})");
         }
