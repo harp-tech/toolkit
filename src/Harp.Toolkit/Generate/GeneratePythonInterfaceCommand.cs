77575ccd6d86723b9a49510d1b5457bde830df5d
glopesdev <g.lopes@neurogears.org>
2026-09-21 14:45:18 +0100

    Add a package option to the Python interface (#49)
    With the new --package option the interface is generated as __init__.py
    together with a py.typed marker. The output directory alone then
    determines the import path, and a device repository needs no re-export
    module. The default still produces a single device.py and no marker.

--- src/Harp.Toolkit/Generate/GeneratePythonInterfaceCommand.cs
+++ src/Harp.Toolkit/Generate/GeneratePythonInterfaceCommand.cs
@@ -11,16 +11,23 @@ class GeneratePythonInterfaceCommand : Command
         MetadataPathArgument metadataPathArgument = new();
         OutputPathOption outputPathOption = new();
 
+        Option<bool> packageOption = new("--package")
+        {
+            Description = "Generate the interface as a package."
+        };
+
         Arguments.Add(metadataPathArgument);
+        Options.Add(packageOption);
         Options.Add(outputPathOption);
 
         SetAction(parseResult =>
         {
             var outputPath = parseResult.GetRequiredValue(outputPathOption);
             var metadataPath = parseResult.GetRequiredValue(metadataPathArgument);
+            var package = parseResult.GetValue(packageOption);
 
             var deviceMetadata = DeviceMetadata.Load(metadataPath.FullName);
-            var generator = new PythonGenerator(deviceMetadata);
+            var generator = new PythonGenerator(deviceMetadata, package);
             var implementation = generator.GenerateImplementation();
             if (GeneratorHelper.AssertNoGeneratorErrors(generator.Errors))
                 GenerateCommand.WriteFileContents(outputPath.FullName, implementation);
